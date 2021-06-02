7475d908243e6e7e1d467ab54488042c272cca1e
consuelita <adelita@operatorfoundation.org>
2021-06-01 19:23:21 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,16 +12,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.3"),
-        .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "1.0.3"),
+        .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "1.0.6"),
         .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.0.12"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.4"),
-        .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "1.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/PacketCaptureBPF.git", from: "0.2.0"),
-        .package(url: "https://github.com/OperatorFoundation/PacketStream.git", from: "0.0.1"),
+        .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "1.1.2"),
+        .package(url: "https://github.com/OperatorFoundation/PacketCaptureBPF.git", from: "0.2.3"),
+        .package(url: "https://github.com/OperatorFoundation/PacketStream.git", from: "0.2.1"),
         .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.1.7"),
-        .package(name: "NetUtils", url: "https://github.com/svdo/swift-netutils.git", from: "4.2.0"),
+        .package(name: "NetUtils", url: "https://github.com/OperatorFoundation/swift-netutils.git", from: "4.3.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.0"),
-        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.11")
+        .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.12")
     ],
     targets: [
         .target(
