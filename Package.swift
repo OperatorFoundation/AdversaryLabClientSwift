a26131889171062799cdb268b00b3f62f9736f2f
consuelita <adelita@operatorfoundation.org>
2021-10-11 17:28:57 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 #if os(macOS)
 let package = Package(
     name: "AdversaryLabClientSwift",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .executable(name: "AdversaryLabClient", targets: ["AdversaryLabClient"]),
         .library(name: "AdversaryLabClientCore", targets: ["AdversaryLabClientCore"]),
@@ -13,14 +13,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.1"),
         .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "1.0.6"),
-        .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.0.12"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.4"),
+        .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.0.13"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
         .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "1.1.2"),
-        .package(url: "https://github.com/OperatorFoundation/PacketCaptureBPF.git", from: "0.2.7"),
+        .package(url: "https://github.com/OperatorFoundation/PacketCaptureBPF.git", from: "0.2.8"),
         .package(url: "https://github.com/OperatorFoundation/PacketStream.git", from: "0.2.1"),
-        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.1.7"),
+        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.2.1"),
         .package(name: "NetUtils", url: "https://github.com/OperatorFoundation/swift-netutils.git", from: "4.3.0"),
-        .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.0"),
+        .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.1"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.12")
     ],
     targets: [
