9710d40752bf5e569d218d1db3971cc5b3e0f65c
consuelita <adelita@operatorfoundation.org>
2021-12-10 19:07:46 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "2.2.0"),
         .package(url: "https://github.com/OperatorFoundation/PacketCaptureBPF.git", from: "0.2.10"),
         .package(url: "https://github.com/OperatorFoundation/PacketStream.git", from: "0.2.1"),
-        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.2.1"),
+        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.2.6"),
         .package(name: "NetUtils", url: "https://github.com/OperatorFoundation/swift-netutils.git", from: "4.3.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.1"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.12")
@@ -59,14 +59,14 @@ let package = Package(
         .library(name: "RawPacket", targets: ["RawPacket"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.2"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.4.1"),
         .package(url: "https://github.com/OperatorFoundation/Bits.git", from: "1.0.3"),
         .package(url:"https://github.com/OperatorFoundation/Chord.git", from: "0.0.13"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.3"),
         .package(url: "https://github.com/OperatorFoundation/InternetProtocols.git", from: "1.1.1"),
         .package(url: "https://github.com/OperatorFoundation/PacketCaptureLibpcap.git", from: "0.2.3"),
         .package(url: "https://github.com/OperatorFoundation/PacketStream.git", from: "0.0.1"),
-        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.1.7"),
+        .package(url: "https://github.com/OperatorFoundation/Song.git", from: "0.2.6"),
         .package(name: "NetUtils", url: "https://github.com/svdo/swift-netutils.git", from: "4.2.0"),
         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.1.0"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.11"),
